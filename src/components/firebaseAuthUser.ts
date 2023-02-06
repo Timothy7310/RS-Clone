f3c028d85484c445876bece64d6ecc52bb31eb75
Anton Haldouski <anton94-94@list.ru>
2023-02-06 18:29:27 +0300

    feat: created api for user authorization

---
+++ src/components/firebaseAuthUser.ts
@@ -0,0 +1,101 @@
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut,
+    updateProfile,
+    deleteUser,
+} from 'firebase/auth';
+
+export default class FirebaseAuthUser {
+    auth;
+
+    constructor() {
+        this.auth = getAuth();
+    }
+
+    registerUser = async (email: string, password: string): Promise<void> => {
+        // const auth = getAuth();
+        await createUserWithEmailAndPassword(this.auth, email, password)
+            .then((userCredential) => {
+                console.log(userCredential);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    };
+
+    logInUser = async (email: string, password: string): Promise<void> => {
+        // const auth = getAuth();
+        await signInWithEmailAndPassword(this.auth, email, password)
+            .then((userCredential) => {
+                console.log(userCredential);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    };
+
+    signOut = async (): Promise<void> => {
+        await signOut(this.auth)
+            .then(() => {
+                // Sign-out successful.
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    };
+
+    updateUser = async (name: string, url:string): Promise<void> => {
+        if (this.auth.currentUser) {
+            await updateProfile(this.auth.currentUser, {
+                displayName: name || this.auth.currentUser.displayName,
+                photoURL: url || this.auth.currentUser.photoURL,
+            })
+                .then(() => {
+                    // Profile updated!
+                    // ...
+                    console.log('User has deleted');
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                    // An error occurred
+                    // ...
+                });
+        }
+    };
+
+    deleteUser = async (): Promise<void> => {
+        const user = this.auth.currentUser;
+
+        if (user) {
+            await deleteUser(user)
+                .then(() => {
+                    // User deleted.
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                    // An error ocurred
+                    // ...
+                });
+        }
+    };
+
+    isUserAuth = async (): Promise<void> => {
+        await onAuthStateChanged(this.auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                // const uid = user.uid;
+                console.log(user);
+                // console.log(this.auth.currentUser);
+                // ...
+            } else {
+                // User is signed out
+                // ...
+                console.log('user log out');
+            }
+        });
+    };
+}
---
+++ src/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyDHzFLnMZibjwbw34-rI4SsD-4hKvkLgm4',
+    authDomain: 'rs-clone-18c8d.firebaseapp.com',
+    projectId: 'rs-clone-18c8d',
+    storageBucket: 'rs-clone-18c8d.appspot.com',
+    messagingSenderId: '182695844224',
+    appId: '1:182695844224:web:d3ea245ffc921f7d964965',
+};
+initializeApp(firebaseConfig);
