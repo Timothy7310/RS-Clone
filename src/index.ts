891bc8c8641601a5a78a04c06cb205ea18c0b5c2
Anton Haldouski <anton94-94@list.ru>
2023-02-06 18:29:27 +0300

    feat: created api for user authorization

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import './index.html';
 import './scss/main.scss';
+import './firebase';
+
+import FirebaseAuthUser from './components/firebaseAuthUser';
+
+const authUser = new FirebaseAuthUser();
+
+// await authUser.registerUser('2abcde@gmail.com', 'qwerty1');
+// await authUser.logInUser('2abcde@gmail.com', 'qwerty1');
+// await authUser.signOut();
+await authUser.isUserAuth();
+// await authUser.deleteUser();
 
 // For example
 // import Controller from './components/controller/controller';
