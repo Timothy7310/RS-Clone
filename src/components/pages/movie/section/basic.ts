90f7426561b8097637e7baf3233f73ac90522d7c
Anastasia Sytsevich <drozdushka@gmail.com>
2023-02-14 01:33:00 +0300

    feat: add component basic for movie page

---
+++ src/components/pages/movie/section/basic.ts
@@ -0,0 +1,27 @@
+import Controller from '../../../controller/controller';
+import about from '../../../templates/movie/about';
+import basicColumn from '../../../templates/movie/basic_column';
+import Component from '../../Component';
+
+export default class Basic {
+    component: Component;
+
+    container: HTMLElement;
+
+    controller: Controller;
+
+    constructor() {
+        this.component = new Component('section', 'basic');
+        this.container = this.component.draw();
+        this.controller = new Controller();
+    }
+
+    async draw(parentContainer: HTMLElement): Promise<void> {
+        const movie = await this.controller.searchMovie('505898', 'id');
+
+        this.container.insertAdjacentHTML('beforeend', basicColumn(movie));
+        this.container.insertAdjacentHTML('beforeend', about(movie));
+        parentContainer.appendChild(this.container);
+        parentContainer.classList.add('movie', 'container');
+    }
+}
