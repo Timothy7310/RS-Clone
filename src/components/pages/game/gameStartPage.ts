9cf3435a3e084eb4ef7b761f5b50ed63c08065e5
Anastasia Sytsevich <drozdushka@gmail.com>
2023-02-26 12:17:15 +0300

    feat: add games paths

---
+++ src/components/pages/game/gameStartPage.ts
@@ -0,0 +1,23 @@
+import Page from '../Page';
+
+export default class GameStart {
+    page: Page;
+
+    container: HTMLElement;
+
+    path: string | undefined;
+
+    constructor(path?: string, id?: string) {
+        this.page = new Page(path);
+        this.container = this.page.draw();
+    }
+
+    draw(): HTMLElement {
+        this.basic.draw(this.container, this.id);
+        return this.container;
+    }
+
+    clear(): void {
+        this.page.clear();
+    }
+}
