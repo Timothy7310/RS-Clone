ecaaeb684b67a15cf6fb860309b90b67da0435e2
Timothy7310 <penlist127@gmail.com>
2023-02-27 03:33:43 +0300

    feat: add tickets page for personal account

---
+++ src/components/templates/userProfile/profileTicketsCards.ts
@@ -0,0 +1,34 @@
+import { TMovie } from '../movie/typesMovie';
+
+const profileTicketsCardsTemplate = (movie: TMovie, item: {
+    date: string;
+    filmID: string;
+    places: string;
+    day: string;
+    time: string;
+}): string => `
+<div class="profile__tickets-info-wrap">
+    <div class="profile__tickets-img-info-wrap">
+        <a href="#/movie/${item.filmID}" class="profile__tickets-img-wrap">
+            <img class="profile__tickets-img" src="${movie?.poster?.previewUrl || movie?.poster?.url || 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Out_Of_Poster.jpg/450px-Out_Of_Poster.jpg'}" alt="">
+        </a>
+        <div class="profile__tickets-title-wrap">
+            <a href="#/movie/${item.filmID}">
+                <h2 class="profile__tickets-title">${movie.name} (${movie.year})</h2>
+            </a>
+            <h3 class="profile__title-english">${movie.alternativeName || ''}</h3>
+            <p class="profile__tickets-timing">
+                <span class="color-orange">${movie.rating.kp}</span> (${movie.votes.kp}) ${movie.movieLength} мин.
+            </p>
+        </div>
+        <div class="profile__tickets-seance-info">
+            <span class="profile__tickets-seance-time">
+                ${item.day}. ${item.time}
+            </span>
+            <span class="profile__tickets-seance-places">Ваши места: ${item.places}</span>
+        </div>
+    </div>
+</div>
+`;
+
+export default profileTicketsCardsTemplate;
--- src/components/types/types.ts
+++ src/components/types/types.ts
@@ -287,4 +287,5 @@ export {
     ImagesType,
     MoviesUserType,
     CountryBoxOfficeType,
+    TicketsProfileType,
 };
